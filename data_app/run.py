f869161b284f6df495b7df78c9961c056f405fd0
tomasbalint <H506VUA@s-mxs.net>
2021-02-12 11:04:31 +0100

    python part working

---
+++ data_app/run.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from datetime import datetime
+
+DATA_FOLDER = './data'
+TIMESTAMP = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+
+def compute_stats():
+    input_stats = pd.read_csv(f'{DATA_FOLDER}/input.csv',header=0)
+    stats = input_stats.groupby(['platform'],as_index=False).agg(
+        count = ('user_id',pd.Series.count),
+        unique = ('user_id',pd.Series.nunique)
+    )
+    stats.to_csv(f'{DATA_FOLDER}/stats_{TIMESTAMP}.csv',index=False,header=True)
+
+
+if __name__ == "__main__":
+    compute_stats()
